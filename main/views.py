b5c3f061bc66c25e36f58d97f9af1ad4ffac0179
ProyectoApolo-Covid <83658530+ProyectoApolo-Covid@users.noreply.github.com>
2021-05-06 19:19:26 +0200

    Add files via upload

--- main/views.py
+++ main/views.py
@@ -1,10 +1,33 @@
-from django.shortcuts import render
-from pathlib import Path
-import numpy as np
 import requests
-import pandas
-import datetime
+from django.shortcuts import render
+from urllib.request import urlopen
+import json
+
 
 def home(request):
-    return render(request, 'index.html')
-# Create your views here.
+    data = []
+    url = "https://covid-193.p.rapidapi.com/statistics"
+
+    querystring = {"country":"Spain"}
+
+    headers = {
+        'x-rapidapi-host': "covid-193.p.rapidapi.com",
+        'x-rapidapi-key': "be7f37114bmsh38c0486c35a5050p1bc1e5jsnf574155ad041"
+        }
+
+    response = requests.request("GET", url, headers=headers, params=querystring).json()
+    
+    d = response['response']
+    s = d[0]
+
+    context = {
+        'all': s['cases']['total'],
+        'recovered': s['cases']['recovered'],
+        'deaths': s['deaths']['total'],
+        'new': s['cases']['new'],
+        'serioz': s['cases']['critical'],
+    }
+    
+   
+
+    return render(request, 'index.html', context)
